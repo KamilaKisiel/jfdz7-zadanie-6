f34c732f24353f628540e9cf4c6ce242efada624
KamilaKisiel <kisiel.kamila@gmail.com>
2017-12-07 20:46:41 +0100

    tasks done. ready to rewiev @pukie

--- zadanie6.js
+++ zadanie6.js
@@ -1,6 +1,4 @@
-// 1. Napisz funkcję, która przyjmuje trzy parametry - (sender, sms, receiver)
-// a następnie zwraca stringa w następującym formacie:
-//     "(FROM: sender) (TO: receiver) (MESSAGE: sms)"
+// 1. 
 
 function textMessage(sender, sms, receiver) {
     return '(FROM:' + sender.charAt(0).toUpperCase()
@@ -11,9 +9,7 @@ function textMessage(sender, sms, receiver) {
 
 console.log(textMessage('janek', '       Nie mogę teraz rozmawiać. Oddzwonię po spotkaniu       ', 'FRANEK'));
 
-// 2. Napisz funkcję, która przyjmuje dwa parametry - firstName oraz lastName,
-// a następnie zwraca inicjały rozdzielone kropką, zwrócony string powinien składać się z wielkich liter.
-// Przykładowy wynik dla następujących zmiennych: var firstName = 'john'; var lastName = 'doe';
+// 2.
 
 function getInitials(firstName, lastName) {
     return firstName.charAt(0).toUpperCase() + '.' + lastName.charAt(0).toUpperCase()
@@ -21,21 +17,14 @@ function getInitials(firstName, lastName) {
 
 console.log(getInitials('john', 'doe'));
 
-// 3. Napisz funkcję, która generuje losową liczbę, zaokrągloną do dwóch liczb po przecinku,
-// a następnie zwraca stringa "Twoja szczęśliwa liczba to: TUTAJ_TA_LOSOWA_LICZBA"
+// 3.
 
 function randomLuckyNumber () {
-    return 'Twoja szczęśliwa liczba to: ' + (Math.round(Math.random()*100)/100)
+    return 'Twoja szczęśliwa liczba to: ' + (Math.random()*100).toFixed(2);
 }
 console.log(randomLuckyNumber());
 
-// 4. Napraw bugi we funkcji oraz popraw formatowanie nazewnictwa. Funkcja powinna zwrocić sumę tip i price
-//
-// Function get_dinner_price {
-//     tip: '20';
-//     tip + price;
-//     var price = 100;
-// }
+// 4.
 
 function getDinnerPrice() {
 
